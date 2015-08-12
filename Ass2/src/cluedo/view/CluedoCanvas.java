85a1d1eea50e1b55708f955c2bd8611de0dbe396
hc09141 <hannahcraighead23@gmail.com>
2015-08-12 21:50:28 +1200

    Board is now background

--- Ass2/src/cluedo/view/CluedoCanvas.java
+++ Ass2/src/cluedo/view/CluedoCanvas.java
@@ -1,12 +1,27 @@
 package cluedo.view;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class CluedoCanvas extends JPanel {
+	
+	Image board;
+	
+	public CluedoCanvas(){
+		board = new ImageIcon("Cluedo-board.jpg").getImage();
+		Dimension maxSize = new Dimension(600,600);
+		setPreferredSize(maxSize);
+	}
 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);		
+	}
+	
+	public void paint(Graphics g) {
+		 g.drawImage(board, 0, 0, null);
 	}
 
 }
