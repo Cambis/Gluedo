4b8b35269e10c4e9a4cf8e1958285b5a8e2112fb
hc09141 <hannahcraighead23@gmail.com>
2015-08-17 14:50:28 +1200

    Grid

--- Ass2/src/cluedo/view/CluedoCanvas.java
+++ Ass2/src/cluedo/view/CluedoCanvas.java
@@ -1,27 +1,50 @@
 package cluedo.view;
 
+import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class CluedoCanvas extends JPanel {
+import cluedo.model.board.Board;
+
+public class CluedoCanvas extends Canvas {
 	
-	Image board;
+	BufferedImage board;
+	int width = 600;
+	int height = 600;
 	
-	public CluedoCanvas(){
-		board = new ImageIcon("Cluedo-board.jpg").getImage();
+	public CluedoCanvas(Board b){
+		File img = new File("clue3.png");		
+		board = new BufferedImage(
+			    width, height, BufferedImage.TYPE_INT_ARGB);	
+		try {
+			board = ImageIO.read(img);
+		} catch (IOException e) {e.printStackTrace();}			
+		
 		Dimension maxSize = new Dimension(600,600);
 		setPreferredSize(maxSize);
 	}
-
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);		
-	}
 	
-	public void paint(Graphics g) {
-		 g.drawImage(board,0 ,0 , null);
+	@Override
+	public void paint(Graphics g){
+		int x  = 25;
+		int y = 23;
+		g.drawImage(board, 0, 0, width, height, 0, 0, board.getWidth(),
+			    board.getHeight(), null);
+		g.setColor(Color.red);
+		for(int i = 0; i < 24; i++){
+			for(int j = 0; j < 25; j++){
+				g.drawRect(i*x + 7 , j*y + 9 , x, y);
+			}
+		}
 	}
 
 }
