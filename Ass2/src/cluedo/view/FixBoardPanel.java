c7a1b579cd3a3e0e32f412b76c48efc6b3955f5e
hc09141 <hannahcraighead23@gmail.com>
2015-08-20 19:00:37 +1200

    More testing

--- Ass2/src/cluedo/view/FixBoardPanel.java
+++ Ass2/src/cluedo/view/FixBoardPanel.java
@@ -1,5 +1,6 @@
 package cluedo.view;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -10,29 +11,54 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class FixBoardPanel extends JPanel {
-	
+
 	int height;
 	int width;
-	Image board;
+	BufferedImage board;
+	BufferedImage titleScreen;
 
 	public FixBoardPanel(){
 		height =  500;
 		width = 500;
-		
-		
-		File img = new File("title-screen.jpg");
+
+
+		File img = new File("Cluedo.jpg");
 		board = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		try {
 			board = ImageIO.read(img);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
+		// Read the board image
+		File img2 = new File("title-screen.jpg");
+		titleScreen = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		try {
+			titleScreen = ImageIO.read(img2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Dimension maxSize = new Dimension(height, width);
+		setPreferredSize(maxSize);
+
+	}
+
+	public void paintBoard(Graphics g){
+		// Draw the board image
+		g.drawImage(board, 0, 0, width, height, 0, 0, board.getWidth(),
+				board.getHeight(), null);
 	}
 	
+	public void paintTitleScreen(Graphics g){
+		// Draw the board image
+		g.drawImage(titleScreen, 0, 0, width, height, 0, 0, titleScreen.getWidth(),
+				titleScreen.getHeight(), null);	
+
+	}
+
 	@Override
 	public void paint(Graphics g){
-		g.drawImage(board, 0, 0, width, height, 0, 0, board.getWidth(null),
-				board.getHeight(null), null);
+		paintTitleScreen(g);
 	}
 }
