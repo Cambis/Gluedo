2273d127f7e11cd49a34c5e01c86c1f9d2591139
hc09141 <hannahcraighead23@gmail.com>
2015-08-21 10:33:43 +1200

    Board now is painting. Turns out a method was overriding and causing it to draw only one square wide.
    Also a fix was made to the PlayerInitFrame to stop it allowing blank
    names

--- Ass2/src/cluedo/view/FixFrame.java
+++ Ass2/src/cluedo/view/FixFrame.java
@@ -7,13 +7,16 @@ import javax.swing.JFrame;
 import cluedo.control.CluedoGame.GameState;
 
 public class FixFrame extends JFrame {
+	
+	//Componenets of the board
+	FixBoardPanel boardCanvas;
 
 	public FixFrame(){
 		super("Cluedo");
 		setLayout(new BorderLayout()); // use border layour
 		this.setSize(800, 800); 
-		FixBoardPanel cbp = new FixBoardPanel();	
-		add(cbp);
+		boardCanvas = new FixBoardPanel();	
+		add(boardCanvas);
 		
 		this.setVisible(true);
 	}
