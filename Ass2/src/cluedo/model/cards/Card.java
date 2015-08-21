7d84f52cb1f3df09da4be52d15e8668c27a01bcb
Cameron Bryers <cambo_the_rambo@hotmail.com>
2015-08-21 12:55:45 +1200

    Added comments to Card, no changes to CluedoGame

--- Ass2/src/cluedo/model/cards/Card.java
+++ Ass2/src/cluedo/model/cards/Card.java
@@ -8,14 +8,18 @@ import cluedo.model.gameObjects.GameObject;
 /**
  * Interface that represents the cards in the game, every game object is
  * represented by a single card.
- * 
- * 
+ *
+ *
  * @author Cameron Bryers, Hannah Craighead
  *
  */
 public interface Card {
 
 	public abstract GameObject getObject();
-	
+
+	/**
+	 * Returns the image of the card as an ImageIcon
+	 * @return
+	 */
 	public abstract ImageIcon getImageIcon();
 }
