b0799fb6720e12a03c4f5985fec58bb02d80153c
Cameron Bryers <cambo_the_rambo@hotmail.com>
2015-08-21 14:54:06 +1200

    Made a player info panel, will have to resize player images

---
+++ Ass2/src/cluedo/view/PlayerInfoPanel.java
@@ -0,0 +1,97 @@
+package cluedo.view;
+
+import java.awt.GridLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import cluedo.model.cards.CharacterCard;
+import cluedo.model.gameObjects.CluedoCharacter;
+import cluedo.model.gameObjects.CluedoCharacter.Suspect;
+
+/**
+ * Shows the player info on the right side of the main frame
+ *
+ * @author Cameron Bryers and Hannah Craighead
+ *
+ */
+public class PlayerInfoPanel extends JPanel {
+
+	// Image of the player's character
+	private JLabel playerIcon, playerLabel;
+
+	// Buttons that the player can choose
+	private JButton[] commandButtons;
+	private ButtonGroup buttonGroup;
+
+	private final String commands[] = new String[] { "Move", "Make A Suggestion",
+			"Make An Accusation", "Show Cards", "Show Checklist" };
+
+	// Current player info
+	private ImageIcon playerImage;
+	private String playerName;
+
+	public PlayerInfoPanel() {
+
+		playerIcon = new JLabel();
+		playerLabel = new JLabel();
+		playerLabel.setHorizontalAlignment(JLabel.CENTER);
+		commandButtons = new JButton[commands.length];
+		buttonGroup = new ButtonGroup();
+
+		init();
+	}
+
+	private void init() {
+
+		setLayout(new GridLayout(10, 1));
+
+		add(playerIcon);
+		add(playerLabel);
+
+		// Set up buttons
+		for (int i = 0; i < commandButtons.length; i++) {
+			commandButtons[i] = new JButton(commands[i]);
+			buttonGroup.add(commandButtons[i]);
+			add(commandButtons[i]);
+		}
+
+		// TODO setup button listeners
+
+	}
+
+	/**
+	 * Change the current player
+	 *
+	 * @param playerImage
+	 * @param playerName
+	 */
+	public void setPlayer(ImageIcon playerImage, String playerName) {
+
+		this.playerImage = playerImage;
+		this.playerName = playerName;
+
+		playerIcon.setIcon(playerImage);
+		playerIcon.setSize(50, 50);
+		playerLabel.setText(playerName);
+	}
+
+	/**
+	 * Testing
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		JFrame frame = new JFrame();
+		ImageIcon image = new CharacterCard(new CluedoCharacter(Suspect.MRS_PEACOCK)).getImageIcon();
+		PlayerInfoPanel panel = new PlayerInfoPanel();
+		panel.setPlayer(image, "David");
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
