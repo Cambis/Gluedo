93b7ead97661feb7320e9e2fff820717647c11db
hc09141 <hannahcraighead23@gmail.com>
2015-08-22 22:20:35 +1200

    Positions for players fixed

--- Ass2/src/cluedo/model/Game.java
+++ Ass2/src/cluedo/model/Game.java
@@ -38,12 +38,12 @@ import cluedo.model.gameObjects.Weapon.WeaponType;
 public class Game {
 
 	// Starting positions for the characters
-	public static final Point MRS_WHITE_START = new Point(0, 9);
-	public static final Point MR_GREEN_START = new Point(0, 14);
-	public static final Point MRS_PEACOCK_START = new Point(6, 24);
-	public static final Point COLONEL_MUSTARD_START = new Point(17, 0);
+	public static final Point MRS_WHITE_START = new Point(9, 0);
+	public static final Point MR_GREEN_START = new Point(14, 0);
+	public static final Point MRS_PEACOCK_START = new Point(7, 23);
+	public static final Point COLONEL_MUSTARD_START = new Point(0, 18);
 	public static final Point MISS_SCARLET_START = new Point(24, 7);
-	public static final Point PROFESSOR_PLUM_START = new Point(19, 24);
+	public static final Point PROFESSOR_PLUM_START = new Point(19, 23);
 
 	public static final boolean DEBUG = false;
 	public static int NUM_OF_DICE;
